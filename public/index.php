9bc3d80b50bf87f27d145f562430ec9c86842b5c
Andraina Rm <andrainaramaro@gmail.com>
2024-12-22 12:55:04 +0300

    update

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 use App\Kernel;
@@ -7,3 +9,6 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+
+
